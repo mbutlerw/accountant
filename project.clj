3abc589d13c870692090dcf1f660732e9c95ed6b
Matt Butler <mattb-w@hotmail.co.uk>
2017-10-02 12:06:51 +0100

    change project name, url and bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject venantius/accountant "0.2.0"
+(defproject mbutlerw/accountant "0.2.1"
   :description "Navigation for Single-Page Applications Made Easy."
-  :url "http://github.com/venantius/accountant"
+  :url "http://github.com/mbutlerw/accountant"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
